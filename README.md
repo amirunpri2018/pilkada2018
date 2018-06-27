6e30117ab9a8d9d94b8cd794ad4e7f806c3087c7
Harry A.M <harry.andriyan@sebangsa.com>
2018-06-27 12:54:29 +0700

    initial comit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pilkada2018
+
+Info Hasil Quick Count Pilkada serentak 2018
+
+## Installation
+### Development
+```
+$ git clone <this_repo>
+
+$ cd <this_repo>
+
+$ npm install
+
+$ npm start
+```
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Pilkada 2018' });
+});
+
+module.exports = router;
